08408cbc54e859425532c0ee6749467d1813dbe9
Taylor Williams <taylor@thewilliams.net>
2018-05-13 01:39:11 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -7,10 +7,11 @@ def prime?(num)
       i = 2
       while i < num
         if (num % i == 0)
-          return true
+          return false
         end
         i += 1
       end
+      return true
     end
     return false
 end
